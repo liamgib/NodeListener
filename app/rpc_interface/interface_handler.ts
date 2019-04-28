8680bb649c05b33ce9c4b07c489c96d49c2025d7
liamgib <40730821+liamgib@users.noreply.github.com>
2019-04-29 02:19:47 +1000

    revised block phrasing algorithm

--- app/rpc_interface/interface_handler.ts
+++ app/rpc_interface/interface_handler.ts
@@ -27,81 +27,52 @@ export class interface_handler{
         return new Promise<Block>(async (resolve, reject) => {
             // Retreive the block hash for the block Height.
             await bitcoin_rpc.call('getblockhash', [blockHeight], async function (err, res) {
-                if (err !== null) {
-                    reject();
-                } else {
-                    //Retrieve the block data from the hash.
-                    await bitcoin_rpc.call('getblock', [res.result, 1], async function (err, res) {
-                        //Map to a block class instance.
-                        if (err !== null) {
-                            reject();
-                        } else {
-                            let newBlock = new Block(blockHeight, res.result.hash, res.result.size, res.result.version, res.result.versionHex, res.result.merkleroot, res.result.time, res.result.nonce, res.result.chainwork);
-                            let icounter = 0;
-                            for (var i = 0, len = res.result.tx.length; i < len; i++) {
-                                //Lookup raw transaction
-                                await bitcoin_rpc.call('getrawtransaction', [res.result.tx[i], 1], async function (err, res) {
-                                    if (err !== null) {
-                                        reject();
-                                    } else {  
-                                        let newTransaction = new Transaction(res.result.txid, res.result.version, res.result.size);
-                                        //Loop through receievers
-                                        let itcounter = 0;
-                                        for(var it = 0, lent = res.result.vout.length; it < lent; it++){
-                                            
-                                            if(res.result.vout[it].scriptPubKey.type !== 'nulldata'){
-                                                newTransaction.addReciever(res.result.vout[it].scriptPubKey.addresses[0], res.result.vout[it].value);
-                                            }
-                                            //Last output
-                                            itcounter++;
-                                            if(itcounter == lent){
-                                                //Loop through senders
-                                                var lena = res.result.vin.length, i = 0;
-                                                for(i; i < lena; i++){
-                                                    if(res.result.vin[i].coinbase !== undefined){
-                                                        newTransaction.addSender('NEWCOINS', newTransaction.getTotalRecieved());
-                                                        if(i + 1 == lena){
-                                                            newBlock.addTransaction(newTransaction);
-                                                            icounter++;
-                                                            if(icounter == len){
-                                                                resolve(newBlock);
-                                                            }
-                                                        }
-                                                    }else{
-                                                        await self.getSenderAddressANDAmount(res.result.vin[i].txid, res.result.vin[i].vout, i, lena).then(result => {
-                                                            newTransaction.addSender(result["address"], result["amount"]);
-                                                            if(result["index"] + 1 == result["max"]){
-                                                                newBlock.addTransaction(newTransaction);
-                                                                icounter++;
-                                                                if(icounter == len){
-                                                                    resolve(newBlock);
-                                                                }
-                                                            }
-                                                        });
-                                                    }
-                                                }
-                                            }
-                                        }
+                if (err !== null) return reject();
+                //Retrieve the block data from the hash.
+                await bitcoin_rpc.call('getblock', [res.result, 1], async function (err, res) {
+                    //Map to a block class instance.
+                    if (err !== null) return reject();
+                    let newBlock = new Block(blockHeight, res.result.hash, res.result.size, res.result.version, res.result.versionHex, res.result.merkleroot, res.result.time, res.result.nonce, res.result.chainwork);
+                    for (var icounter = 0, len = res.result.tx.length; icounter < len; icounter++) {
+                        //Lookup raw transaction
+                        await bitcoin_rpc.call('getrawtransaction', [res.result.tx[icounter], 1], async function (err, res) {
+                            if (err !== null) return reject();  
+                            let newTransaction = new Transaction(res.result.txid, res.result.version, res.result.size);
+                            //Loop through receievers
+                            for(var it = 0, lent = res.result.vout.length; it < lent; it++){
+                                if(res.result.vout[it].scriptPubKey.type !== 'nulldata') newTransaction.addReciever(res.result.vout[it].scriptPubKey.addresses[0], res.result.vout[it].value);
+                                if(it == lent - 1) for(var i = 0, lena = res.result.vin.length; i < lena; i++){
+                                    if(res.result.vin[i].coinbase !== undefined){
+                                        newTransaction.addSender('NEWCOINS', newTransaction.getTotalRecieved());
+                                        if(i + 1 == lena) await newBlock.addTransaction(newTransaction);
+                                        if(icounter == len) resolve(newBlock);
+                                        
+                                    }else{
+                                        await self.getSenderAddressANDAmount(res.result.vin[i].txid, res.result.vin[i].vout).then(async result => {
+                                            newTransaction.addSender(result["address"], result["amount"]);
+                                            if(i + 1 == lena) await newBlock.addTransaction(newTransaction);
+                                            if(icounter == len) resolve(newBlock);
+                                        });
                                     }
-                                });
-                            } 
-                        }  
-                    });
-                }
+                                }
+                            }
+                        });
+                    } 
+                });
             });
         });
     }
 
-    public async getSenderAddressANDAmount(txid:string, vout:number, i:number, max:number) {
+    public async getSenderAddressANDAmount(txid:string, vout:number) {
         var self = this;
         return new Promise<Object>((resolve, reject) => {
             bitcoin_rpc.call('getrawtransaction', [txid, 1], async function (err, resa) {
                 if(err !== null){
                      setTimeout(function(){
-                        resolve(self.getSenderAddressANDAmount(txid, vout, i, max));
+                        resolve(self.getSenderAddressANDAmount(txid, vout));
                      }, 100);
                 }else{
-                    resolve({index: i, max: max, address: resa.result.vout[vout].scriptPubKey.addresses[0], amount: resa.result.vout[vout].value});
+                    resolve({address: resa.result.vout[vout].scriptPubKey.addresses[0], amount: resa.result.vout[vout].value});
                 }
             });
         });
